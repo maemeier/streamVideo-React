0bfa89368e66086de8a97eb85a130a815156ca62
Mae Kangsadan <maemeier@live.com>
2020-02-14 09:27:16 +0100

    i- redux form

--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ import { createStore, applyMiddleware, compose } from "redux";
 import App from "./component/App";
 import reducers from "./reducers";
 
-const composeEnhancers =
-  window.__REDUX__DEVTOOLS__EXTENSTION_COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(reducers, composeEnhancers(applyMiddleware()));
 ReactDOM.render(
